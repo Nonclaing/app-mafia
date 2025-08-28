4dcbb6be881ea97d3bf1f2bb5919647608848c9f
ZvezdilinAK <artem@intervolga.ru>
2025-08-28 11:13:09 +0300

    [develop] Обновил пакеты

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,10 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
     "@nuxt/image",
     "@nuxt/eslint",
+    "@nuxt/test-utils/module",
+    ["nuxt-schema-org", {
+      canonicalHost: "https://localhost:3000",
+    }],
     ["@nuxt/fonts", {
       defaults: {
         weights: ["100 900"],
@@ -44,22 +48,71 @@ export default defineNuxtConfig({
       },
     }],
   ],
-  ssr: false,
+  ssr: true,
   imports: {
     imports: [
       { from: "es-toolkit/compat", name: "get", as: "useGet" },
       { from: "es-toolkit/compat", name: "map", as: "useMap" },
       { from: "es-toolkit/compat", name: "find", as: "useFind" },
       { from: "es-toolkit/compat", name: "keys", as: "useKeys" },
+      { from: "es-toolkit/compat", name: "keyBy", as: "useKeyBy" },
       { from: "es-toolkit/compat", name: "merge", as: "useMerge" },
+      { from: "es-toolkit/compat", name: "uniqueId", as: "useUniqueId" },
+      { from: "es-toolkit/compat", name: "join", as: "useJoin" },
+      { from: "es-toolkit/compat", name: "mapValues", as: "useMapValues" },
+      { from: "es-toolkit/compat", name: "filter", as: "useFilter" },
+      { from: "es-toolkit/compat", name: "castArray", as: "useCastArray" },
+      { from: "es-toolkit/compat", name: "reject", as: "useReject" },
+      { from: "es-toolkit/compat", name: "split", as: "useSplit" },
+      { from: "es-toolkit/compat", name: "size", as: "useSize" },
+      { from: "es-toolkit/compat", name: "chunk", as: "useChunk" },
+      { from: "es-toolkit/compat", name: "forEach", as: "useForEach" },
+      { from: "es-toolkit/compat", name: "sortBy", as: "useSortBy" },
+      { from: "es-toolkit/compat", name: "groupBy", as: "useGroupBy" },
+      { from: "es-toolkit/compat", name: "range", as: "useRange" },
+      { from: "es-toolkit/compat", name: "findIndex", as: "useFindIndex" },
+      { from: "es-toolkit/compat", name: "remove", as: "useRemove" },
+      { from: "es-toolkit/compat", name: "max", as: "useMax" },
+      { from: "es-toolkit/compat", name: "pick", as: "usePick" },
+      { from: "es-toolkit/compat", name: "omit", as: "useOmit" },
+      { from: "es-toolkit/compat", name: "values", as: "useValues" },
+      { from: "es-toolkit/compat", name: "throttle", as: "useThrottle" },
+      { from: "es-toolkit/compat", name: "keyBy", as: "useKeyBy" },
+      { from: "es-toolkit/compat", name: "compact", as: "useCompact" },
+      { from: "es-toolkit/compat", name: "castArray", as: "useCastArray" },
+      { from: "es-toolkit/compat", name: "join", as: "useJoin" },
+      { from: "es-toolkit/compat", name: "isArray" },
       { from: "es-toolkit/compat", name: "isEmpty" },
-      { from: "es-toolkit/compat", name: "toString" },
+      { from: "es-toolkit/compat", name: "isEqual" },
+      { from: "es-toolkit/compat", name: "isNull" },
     ],
   },
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Ищу дизайнера. 59300+ дизайнеров: портфолио и резюме",
+      meta: [
+        { name: "description", content: "Ищу дизайнера — это проект, объединивший тысячи дизайнеров. Ищем дизайнеров для клиентов каждый день" },
+        { name: "keywords", content: "Ищу дизайнера, вакансии для дизайнеров, designers, дизайнеры работа, сайт дизайнеров, работа для дизайнеров, биржа дизайнеров" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.ico" },
+      ],
+    },
+  },
   css: [
     "~/assets/css/tailwind.css",
   ],
+  runtimeConfig: {
+    apiBaseUrl: process.env.NUXT_API_BASE_URL || "http://localhost:3000", // серверный
+    public: {
+      apiBaseUrl: process.env.NUXT_API_BASE_URL || "http://localhost:3000", // доступен и на клиенте
+    },
+  },
+  routeRules: {},
+  devServer: {
+    host: "0.0.0.0",
+  },
   compatibilityDate: "2025-07-15",
   vite: {
     plugins: [
