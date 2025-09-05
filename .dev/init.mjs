ad56d215cb0111b3f470a6bdcd39c3a5b0317f60
ZvezdilinAK <artem@intervolga.ru>
2025-09-05 11:02:26 +0300

    [develop] Генерация базовой db через faker

--- .dev/init.mjs
+++ .dev/init.mjs
@@ -1,18 +1,13 @@
 import { writeFile } from "fs/promises";
+import { map, range } from "es-toolkit/compat";
+import { fakerRU } from '@faker-js/faker';
 
 const data = {
-  items: {
-    1: {
-      id: "1",
-      name: "Sample item 1",
-      description: "This is a sample item",
-    },
-    2: {
-      id: "2",
-      name: "Sample item 2",
-      description: "This is a sample item",
-    },
-  },
+  items: map(range(10), (id) => ({
+    id,
+    name: fakerRU.commerce.productName(),
+    description: fakerRU.commerce.productDescription(),
+  })),
 };
 
 await writeFile(".dev/db.json", JSON.stringify(data, null, 2));
