a47e49eb4787f34787fff0d5bb064c298eec3830
AlexanderShchedrin <shchedrin@intervolga.ru>
2026-03-25 18:04:47 +0300

    [LEARN-313] - добавил сплеши и фон сплеша

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,37 +9,40 @@ const config: CapacitorConfig = {
     allowNavigation: ["/rest/**", "habr.com"],
   },
   plugins: {
-    plugins: {
-      CapacitorUpdater: {
-        autoUpdate: false,
-      },
+    // OTA (hot-updates) обновления
+    CapacitorUpdater: {
+      autoUpdate: false,
     },
+    // Работа с http
     CapacitorHttp: {
       enabled: true,
     },
+    // Работа с куками
     CapacitorCookies: {
       enabled: true,
     },
+    // Отключение базового статусбара
     SystemBars: {
+      style: Style.Dark,
       insetsHandling: "disable",
     },
-    Keyboard: {
-      resizeOnFullScreen: false,
-    },
+    // Плагина для резервирования пространства у статус бара
     EdgeToEdge: {
       backgroundColor: "#0f172b",
       navigationBarColor: "#0f172b",
       statusBarColor: "#0f172b",
     },
+    // Настройка статусбара в старых android
     StatusBar: {
       overlaysWebView: false,
-      style: Style.Light,
+      style: Style.Dark,
       backgroundColor: "#0f172b",
     },
+    // Начальный экран открытия приложения
     SplashScreen: {
       launchAutoHide: true,
       launchFadeOutDuration: 500, // плавное затухание (опционально)
-      backgroundColor: "#0f172b", // цвет фона под сплешем
+      backgroundColor: "#ffffff", // цвет фона под сплешем
       androidScaleType: "CENTER_CROP", // очень важно для Android
       splashFullScreen: true,
       splashImmersive: true, // убирает статус-бар и навигацию
